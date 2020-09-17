2c0d05d154260f75294393cda8b21a4156aa5fa4
sam <sam@freighttrust.com>
2020-09-17 12:55:45 -0700

    build(deployment): fix netlify deployment
    gitignore and toml fixes

---
+++ build/precache-manifest.edd53bf956424a1445903d67c4532a67.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b803484d5827d7c5d87e5912d49f8588",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8a79e0019321a450e97",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dd89b300e453e78af54a",
+    "url": "/static/css/main.fbaa3b38.chunk.css"
+  },
+  {
+    "revision": "a8a79e0019321a450e97",
+    "url": "/static/js/2.c645df9f.chunk.js"
+  },
+  {
+    "revision": "b526e248f58b11e80e5329151c067646",
+    "url": "/static/js/2.c645df9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd89b300e453e78af54a",
+    "url": "/static/js/main.50cad72a.chunk.js"
+  },
+  {
+    "revision": "d023545a91114e57fd62",
+    "url": "/static/js/runtime-main.8236ad6a.js"
+  },
+  {
+    "revision": "d59d2eeb29247b4bbd8156c5300e7b8a",
+    "url": "/static/media/Logo_Aave.d59d2eeb.png"
+  },
+  {
+    "revision": "45d4fd809e2461450550724e71b9f941",
+    "url": "/static/media/Logo_Compound.45d4fd80.png"
+  },
+  {
+    "revision": "f2caca99842ea12dec56fbb33bc72bfe",
+    "url": "/static/media/Logo_ERC20.f2caca99.png"
+  },
+  {
+    "revision": "a6fe5fb15540a749d64809c480683ae1",
+    "url": "/static/media/Logo_Kyber.a6fe5fb1.png"
+  },
+  {
+    "revision": "7163c11eaf363f844e2e7ac77d76194f",
+    "url": "/static/media/Logo_Maker.7163c11e.png"
+  },
+  {
+    "revision": "a96dee376e6640084983f46393104084",
+    "url": "/static/media/Logo_Sets.a96dee37.png"
+  },
+  {
+    "revision": "6e641b6e8c6b24a9f3b9798ee2deebb9",
+    "url": "/static/media/Logo_Uniswap.6e641b6e.png"
+  },
+  {
+    "revision": "c69913c1eea546e75b985783bd6ad8ae",
+    "url": "/static/media/Logo_YFI.c69913c1.png"
+  }
+]);
