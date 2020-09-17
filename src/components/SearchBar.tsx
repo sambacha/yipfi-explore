0aba2e867214f0a23d42661221561099c121cf99
sam <sam@freighttrust.com>
2020-09-16 21:02:35 -0700

    update pkg deps
    Signed-off-by: sam <sam@freighttrust.com>

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -18,7 +18,7 @@ export const SearchBar = () => {
             <div>
                 <form>
                     <span className="small text-muted">e.g. 0x932fc4fd0eEe66F22f1E23fBA74D7058391c0b15</span>
-                    <input className="form-control" placeholder="Enter contract address.." 
+                    <input className="form-control" placeholder="Discover markets..." 
                             value={contractAddress} onChange={e => setContractAddress(e.target.value)} />
 
                     <button type="submit" className="btn btn-info mt-3" onClick={handleClick}>Search</button>
