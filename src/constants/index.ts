560a51212051392bf0a4f14822d0d871a25a30eb
sam <sam@freighttrust.com>
2020-09-16 21:11:30 -0700

    netlify: toml config
    Signed-off-by: sam <sam@freighttrust.com>

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export const ETHERSCAN_ADDRESS_LINK = "https://etherscan.io/address/";
 export const ETHERSCAN_TX_LINK = "https://etherscan.io/tx/";
-export const ETHERSCAN_API_KEY = "";
+export const ETHERSCAN_API_KEY = "D2WHM1W583BM53BK3DX23Y2JGWYRSAPWZM";
